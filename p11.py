c2e06ad3131da4d1f86998c820b40e50a292981b
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-15 16:49:56 +0530

    Add files via upload

---
+++ p11.py
@@ -0,0 +1,18 @@
+# wapp to find type of given function
+a = 12
+print(a, type(a))
+
+b=1.34
+print(b, type(b))
+
+c='harshu'
+print(c, type(c))
+
+d="patil"
+print(d, type(d))
+
+e= True
+print(e, type(e))
+
+f= False
+print(f, type(f))
